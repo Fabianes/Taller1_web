688e3c95d4ddd62fbf492c07f3f92b8a6bd32268
Unknown <dnavarro@udec.cl>
2018-11-02 19:55:17 -0300

    punto 2 entero de listo! (creo)

--- taller1p2/taller1p2/spiders/example.py
+++ taller1p2/taller1p2/spiders/example.py
@@ -6,14 +6,13 @@ class InfSpider(scrapy.Spider):
 	name = 'InfSpider'
 	#allowed_domains = ['inf.ucv.cl/']
 	start_urls = ['http://www.inf.ucv.cl/']
-	#httpS
-
 
 	def parse(self, response):
 		hxs = scrapy.Selector(response)
 		all_links = hxs.xpath('*//a/@href').extract()
+
 		for link in all_links:
-			#print link.encode("utf-8")
+
 			if link.encode("utf-8") != "#":
 				if (link.startswith('http://') or link.startswith('https://')) :
 					#print link.encode("utf-8")
@@ -21,12 +20,24 @@ class InfSpider(scrapy.Spider):
 					yield scrapy.Request(link.encode("utf-8"), method='HEAD', callback=self.urlInfo, meta={ 'dont_retry': True, 'handle_httpstatus_list': [302]}, dont_filter = True )
 	
 	def urlInfo(self, response):
-#		print response.headers
 
-		yield Taller1P2Item(
-			URL = response.url,
-			status = response.status,
-			content_type = response.headers['Content-Type'],
-			#content_length = len(response.body)
-			content_length = int(response.headers['Content-Length'])
-		)
+		if response.headers['Content-Type'] in "text/html":
+
+			if response.headers.get('Content-Length') is None:
+
+				yield Taller1P2Item(
+					URL = response.url,
+					status = response.status,
+					content_type = response.headers['Content-Type'],
+					content_length = 0
+					#content_length = int(response.headers['Content-Length'])
+				)
+
+			elif response.headers.get('Content-Length') is not None:
+
+				yield Taller1P2Item(
+					URL = response.url,
+					status = response.status,
+					content_type = response.headers['Content-Type'],	
+					content_length = int(response.headers['Content-Length'])
+				)
